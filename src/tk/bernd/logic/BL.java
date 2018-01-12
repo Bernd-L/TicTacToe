28284b392e7459297c9e89c89ded712b619f055e
Bernd L <bernd.lafer@gmail.com>
2018-01-12 12:47:14 +0100

    v 1.0

--- src/tk/bernd/logic/BL.java
+++ src/tk/bernd/logic/BL.java
@@ -1,48 +1,147 @@
 package tk.bernd.logic;
 
+
+import javax.swing.*;
 import java.awt.*;
+import java.util.Random;
 import java.util.stream.IntStream;
 
 public class BL {
 
-    public BL(Frame frame){
-        this.frame = frame;
-    }
-    private final Frame frame;
-    public static final int CROSS_WIN = -3;
-    public static final int DEFAULT = 0;
-    public static final int CIRCLE_WIN = 3;
+    //Players
     public static final int CROSS = -1;
+    public static final int DEFAULT = 0;
     public static final int CIRCLE = 1;
+
+    //Status
+    public static final int RUNNING_GAME = 4;
+    public static final int DRAW = 5;
+
+    //Win sums
+    public static final int CROSS_WIN = -3;
+    public static final int CIRCLE_WIN = 3;
+
+    //Text
+    public static final String CROSS_TEXT = "X";
+    public static final String CIRCLE_TEXT = "O";
+
+    private final Frame frame;
+
+    Random r = new Random();
+
+    private int turn = DEFAULT;
 
     private int[][] currentSet = new int[3][3]; //[LEFT/RIGHT] [UP/DOWN]
 
-    public int checkWin(){
+    public BL(Frame frame) {
+        this(frame, DEFAULT);
+    }
+
+    public BL(Frame frame, int beginner) {
+        this.frame = frame;
+        switch (beginner) {
+            case CROSS:
+                turn = CROSS;
+                break;
+            case DEFAULT:
+                turn = r.nextBoolean() ? CROSS : CIRCLE;
+                break;
+            case CIRCLE:
+                turn = CIRCLE;
+                break;
+            default:
+                throw new IndexOutOfBoundsException();
+        }
+    }
+
+    public int checkWin() {
+        boolean nothingIsEmpty = true;
         //Check for y axis wins
         for (int i = 0; i < 3; i++) {
-            switch (IntStream.of(currentSet[i]).sum()){
-                case CROSS:
-                    return CROSS;
+            switch (IntStream.of(currentSet[i]).sum()) {
+                case CROSS_WIN:
+                    return CROSS_WIN;
                 case DEFAULT:
                     break;
-                case CIRCLE:
-                    return CIRCLE;
+                case CIRCLE_WIN:
+                    return CIRCLE_WIN;
                 default:
-                    throw new AssertionError();
             }
         }
 
-
+        //Check for x axis wins
         for (int i = 0; i < 3; i++) {
             int sum = 0;
             for (int j = 0; j < 3; j++) {
-                sum += currentSet[]
+                sum += currentSet[j][i];
+                if (currentSet[j][i] == DEFAULT)
+                    nothingIsEmpty = false;
+            }
+            switch (sum) {
+                case CROSS_WIN:
+                    return CROSS_WIN;
+                case CIRCLE_WIN:
+                    return CIRCLE_WIN;
+                case DEFAULT:
+                    break;
+                default:
             }
         }
 
+        //Check diagonal from low to high y axis
+        switch (currentSet[0][0] + currentSet[1][1] + currentSet[2][2]) {
+            case CIRCLE_WIN:
+                return CIRCLE_WIN;
+            case CROSS_WIN:
+                return CROSS_WIN;
+            case DEFAULT:
+                break;
+            default:
+        }
 
-        return 10;
+        //Check diagonal from high to low y axis
+        switch (currentSet[0][2] + currentSet[1][1] + currentSet[2][0]) {
+            case CIRCLE_WIN:
+                return CIRCLE_WIN;
+            case CROSS_WIN:
+                return CROSS_WIN;
+            case DEFAULT:
+                break;
+        }
+
+        if (nothingIsEmpty)
+            return DRAW;
+
+        return RUNNING_GAME;
     }
 
-    public void setSign(int x, int y)
+    public void setSign(int x, int y, int sign) {
+        if (sign != CIRCLE | sign != CROSS) {
+            throw new IndexOutOfBoundsException();
+        }
+        currentSet[x][y] = sign;
+    }
+
+    public int handlePress(int x, int y, JButton btn) {
+        if (currentSet[x][y] != DEFAULT)
+            return RUNNING_GAME;
+        currentSet[x][y] = turn;
+        btn.setText(turn == CIRCLE ? CIRCLE_TEXT : CROSS_TEXT);
+        btn.setBackground(turn == CROSS ? Color.red : Color.blue);
+
+        int cw = checkWin();
+        if (cw == RUNNING_GAME)
+        turn = turn == CIRCLE ? CROSS : CIRCLE;
+            return cw;
+
+    }
+
+    public void restart(int beginner) {
+        if (beginner == DEFAULT){
+            turn = turn == CIRCLE ? CROSS : CIRCLE;
+        }else{
+            turn = beginner;
+        }
+        currentSet = new int[3][3];
+    }
 }
--- src/tk/bernd/view/MainWindow.java
+++ src/tk/bernd/view/MainWindow.java
@@ -1,45 +1,15 @@
 package tk.bernd.view;
 
-import javax.swing.*;
-import tk.bernd.logic.*;
+import tk.bernd.logic.BL;
 
+import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class MainWindow {
 
-    BL bl = new BL();
-
-    public MainWindow() {
-        button1.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent actionEvent) {
-                bl
-            }
-        });
-    }
-
-    public static void main(String[] args) {
-        JFrame frame = new JFrame("MainWindow");
-        frame.setContentPane(new MainWindow().pnGameField);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Windows".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException | InstantiationException | UnsupportedLookAndFeelException | IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(frame.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-
-
-        frame.pack();
-        frame.setVisible(true);
-    }
-
+    BL bl = new BL(new Frame());
     private JPanel pnGameField;
     private JButton button1;
     private JButton button2;
@@ -50,4 +20,132 @@ public class MainWindow {
     private JButton button7;
     private JButton button8;
     private JButton button9;
+    private int circleWins = 0;
+    private int crossWins = 0;
+    private int draws = 0;
+
+    MainWindow(Frame frame) {
+        this.bl = new BL(frame);
+    }
+    public MainWindow() {
+        button1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                handleClick(0, 2, button1);
+            }
+        });
+        button2.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                handleClick(1, 2, button2);
+            }
+        });
+        button3.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                handleClick(2, 2, button3);
+            }
+        });
+        button4.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                handleClick(0, 1, button4);
+            }
+        });
+        button5.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                handleClick(1, 1, button5);
+            }
+        });
+        button6.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                handleClick(2, 1, button6);
+            }
+        });
+        button7.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                handleClick(0, 0, button7);
+            }
+        });
+        button8.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                handleClick(1, 0, button8);
+            }
+        });
+        button9.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                handleClick(2, 0, button9);
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("TicTacToe by Bernd-L");
+        new MainWindow(frame);
+        frame.setContentPane(new MainWindow().pnGameField);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Windows".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException | InstantiationException | UnsupportedLookAndFeelException | IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(frame.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    private void handleClick(int x, int y, JButton btn) {
+        switch (bl.handlePress(x, y, btn)) {
+            case BL.CIRCLE_WIN:
+                circleWins++;
+                JOptionPane.showMessageDialog(null, "Circle won!\n\nCircle:  " + circleWins + "\nCross:  " + crossWins + "\nDraws: " + draws);
+                restart();
+                break;
+            case BL.CROSS_WIN:
+                crossWins++;
+                JOptionPane.showMessageDialog(null, "Cross won!\n\nCircle:  " + circleWins + "\nCross:  " + crossWins + "\nDraws: " + draws);
+                restart();
+                break;
+            case BL.DRAW:
+                draws++;
+                JOptionPane.showMessageDialog(null, "It's a draw!\n\nCircle:  " + circleWins + "\nCross:  " + crossWins + "\nDraws: " + draws);
+                restart();
+        }
+    }
+
+    private void restart() {
+        restart(BL.DEFAULT);
+    }
+
+    private void restart(int beginer) {
+        bl.restart(beginer);
+        button1.setBackground(null);
+        button2.setBackground(null);
+        button3.setBackground(null);
+        button4.setBackground(null);
+        button5.setBackground(null);
+        button6.setBackground(null);
+        button7.setBackground(null);
+        button8.setBackground(null);
+        button9.setBackground(null);
+
+        button1.setText(" ");
+        button2.setText(" ");
+        button3.setText(" ");
+        button4.setText(" ");
+        button5.setText(" ");
+        button6.setText(" ");
+        button7.setText(" ");
+        button8.setText(" ");
+        button9.setText(" ");
+    }
 }
